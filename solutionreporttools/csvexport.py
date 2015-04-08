d24d9526fe0b777da8eb9d9ceb285288f37b7c46
Michael Miller <mmiller@esri.com>
2015-04-07 22:21:56 -0400

    updated cvs export to use DA search cursor and new way to check for shape length/area fields.

--- solutionreporttools/csvexport.py
+++ solutionreporttools/csvexport.py
@@ -8,16 +8,35 @@
     @copyright: Esri, 2015
     @originial source of script is from http://mappatondo.blogspot.com/2012/10/this-is-my-python-way-to-export-feature.html with modifications
 """
-import sys, os, arcpy, csv, codecs, commands
+import sys, arcpy, csv
 from arcpy import env
-
+class ReportToolsError(Exception):
+    """ raised when error occurs in utility module functions """
+    pass
+def trace():
+    """
+        trace finds the line, the filename
+        and error message and returns it
+        to the user
+    """
+    import traceback, inspect
+    tb = sys.exc_info()[2]
+    tbinfo = traceback.format_tb(tb)[0]
+    filename = inspect.getfile(inspect.currentframe())
+    # script name + line number
+    line = tbinfo.split(", ")[1]
+    # Get Python syntax error
+    #
+    synerror = traceback.format_exc().splitlines()[-1]
+    return line, filename, synerror
 class CSVExport:
     _tempWorkspace = None
     _tempFeature = None
     _CSVLocation = None
        
-    def __init__(self,configParams=""):
-        # Gets the values of where the temp feature class resides and the output location of the CSV.
+    def __init__(self, configParams=""):
+        # Gets the values of where the temp feature class resides and
+        # the output location of the CSV.
         try:
             if configParams and configParams != "":
                 
@@ -29,13 +48,30 @@ class CSVExport:
                 
                 self._tempFeature = "CSVTemp"
                     
-                return None
+
             else:
                 print "Error, no config file path specified."
-                return False        
+                
+        except arcpy.ExecuteError:
+            line, filename, synerror = trace()
+            raise ReportToolsError({
+                "function": "create_report_layers_using_config",
+                "line": line,
+                "filename":  filename,
+                "synerror": synerror,
+                "arcpyError": arcpy.GetMessages(2),
+            }
+            )
         except:
-            print "Unexpected error initializing CSV report:", sys.exc_info()[0]
-            return False  
+            line, filename, synerror = trace()
+            raise ReportToolsError({
+                "function": "create_report_layers_using_config",
+                "line": line,
+                "filename":  filename,
+                "synerror": synerror,
+            }
+            )
+          
         
     def WriteCSV(self):
         # This function writes the CSV. It writes the header then the rows. This script omits the SHAPE fields.
@@ -45,44 +81,66 @@ class CSVExport:
             fc = arcpy.ListFeatureClasses(self._tempFeature)
             for fcs in fc:      
                     
-                    outFile = open(self._CSVLocation, 'wb')      
+                outFile = open(self._CSVLocation, 'wb')      
             
-                    linewriter = csv.writer(outFile, delimiter = ',')
-                    
-                    fcdescribe = arcpy.Describe(fcs)
-                    flds = fcdescribe.Fields
-            
-                    header = []
-                    for fld in flds:
-                        value = fld.AliasName
-                        #if value.upper() == 'SHAPE':
-                        if (value.upper() == "SHAPE") | (value.upper() == "SHAPE_LENGTH") | (value.upper() == "SHAPE_AREA"):
-                            continue
-                        else:
-                            fullHeader = value
-                            header.append(fullHeader)
-                    linewriter.writerow(header)
-            
-                    cursor = arcpy.SearchCursor(fcs)
-                    row = cursor.next()
-            
-                    while row:
+                linewriter = csv.writer(outFile, delimiter = ',')
+                
+                fcdescribe = arcpy.Describe(fcs)
+                flds = fcdescribe.Fields
+        
+                header = []
+                resFields = []
+                if hasattr(fcdescribe, 'areaFieldName'):
+                    resFields.append(fcdescribe.areaFieldName)
+                if hasattr(fcdescribe, 'lengthFieldName'):
+                    resFields.append(fcdescribe.lengthFieldName)
+                if hasattr(fcdescribe, 'shapeFieldName'):
+                    resFields.append(fcdescribe.shapeFieldName)                
+                
+                fldLst = []
+                for fld in flds:
+                    value = fld.AliasName
+                    #if value.upper() == 'SHAPE':
+                    if (fld.name in resFields):
+                        pass
+                    else:
+                        fullHeader = value
+                        header.append(fullHeader)
+                        fldLst.append(fld.name)
+                linewriter.writerow(header)
+               
+                with arcpy.da.SearchCursor(fcs,fldLst) as rows:
+                    for row in rows:
                         line = []
-                        for fld in flds:
-                            value = row.getValue(fld.Name)
-                            if (fld.Name.upper() == "SHAPE") | (fld.Name.upper() == "SHAPE_LENGTH") | (fld.Name.upper() == "SHAPE_AREA"):
-                            #if str(value).startswith("<geo"):
-                                continue
-                            else:
-                                line.append(value)
-                        
-                        linewriter.writerow([s.encode('utf8') if type(s) is unicode else s for s in line])
-                        del line
-                        row = cursor.next()
+                        for i in range(0,len(fldLst)):
+                                           
+                            value = row[i]
+                            
+                            line.append(value)
                     
-                    del cursor
-                    outFile.close() 
+                        linewriter.writerow([s.encode('utf8') if isinstance(s,unicode) else s for s in line])
+                    del line
+                   
+                    del row
+                    del rows
+                outFile.close() 
             return True
+        except arcpy.ExecuteError:
+            line, filename, synerror = trace()
+            raise ReportToolsError({
+                "function": "create_report_layers_using_config",
+                "line": line,
+                "filename":  filename,
+                "synerror": synerror,
+                "arcpyError": arcpy.GetMessages(2),
+            }
+            )
         except:
-            print "Unexpected error creating CSV report:", sys.exc_info()[0]
-            return False         
+            line, filename, synerror = trace()
+            raise ReportToolsError({
+                "function": "create_report_layers_using_config",
+                "line": line,
+                "filename":  filename,
+                "synerror": synerror,
+            }
+            )
